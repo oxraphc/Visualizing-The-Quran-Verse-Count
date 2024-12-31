663f63e25a06264866a2295044a59b4ff5a0f756
Hendra Manudinata <manoedinata@gmail.com>
2024-12-31 14:26:29 +0700

    Update source data and adapt the parsing method
    * We don't need that regex anymore!
    * Do some code cleanups

--- scrap_wiki.py
+++ scrap_wiki.py
@@ -3,31 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-target_webpage_link = "https://en.wikipedia.org/wiki/List_of_chapters_in_the_Quran"
+target_webpage_link = "https://id.wikipedia.org/wiki/Surah"
 
 target_webpage = requests.get(target_webpage_link)
 
 soup = BeautifulSoup(target_webpage.text, "html.parser")
 
 def scrap_table():
-    table = soup.find("table")
-    rows = table.find_all("tr")
-    
+    table = soup.find("table", class_="wikitable")
+    rows = table.find_all("tr")[1:] # Skip first row
+
     Quran_data = []
     for row in rows:
         column = row.find_all("td")
-        if column == []:
-            pass
-        else:
-            surah_index = column[0].text.strip()
-            surah_name = re.search(r"^[^(\s]+", column[1].text).group()
-            # RegEx expression used to better extract surah name only, excluding any subtitutes or translation
-            number_of_verses = re.search(r"^\d+", column[4].text).group()
-            # RegEx expression used to extract number of verses only
-            Quran_data.append(f'{surah_index},{surah_name},{number_of_verses}')
-        
+        surah_index = column[0].text.strip()
+        surah_name = column[1].text.split("Surah")[1].strip()
+        number_of_verses = column[4].text.strip()
+        Quran_data.append(f'{surah_index},{surah_name},{number_of_verses}')
+
     convert_to_csv = "\n".join(Quran_data)
     return convert_to_csv
 
-file = open("Quran Data.csv", "w")
-file.write(scrap_table())
+with open("Quran Data.csv", "w") as file:
+    file.write(scrap_table())
